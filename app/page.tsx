62f9f6344277f22768fb5b9f1634c7e3219fcb58
phuoc <phuoc.anonydev2k3@gmail.com>
2024-09-13 18:06:54 +0700

    feat: update ckeditor

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,198 @@
+"use client"
+
 import Image from "next/image";
+import {CKEditor} from '@ckeditor/ckeditor5-react';
+import {
+  ClassicEditor,
+  Autoformat,
+  Bold,
+  Italic,
+  Underline,
+  BlockQuote,
+  Base64UploadAdapter,
+  CloudServices,
+  Essentials,
+  Heading,
+  Image as ckImage,
+  ImageCaption,
+  ImageResize,
+  ImageStyle,
+  ImageToolbar,
+  ImageUpload,
+  PictureEditing,
+  Indent,
+  IndentBlock,
+  Link,
+  List,
+  MediaEmbed,
+  Mention,
+  Paragraph,
+  PasteFromOffice,
+  Table,
+  TableColumnResize,
+  TableToolbar,
+  TextTransformation,
+  SimpleUploadAdapter
+} from 'ckeditor5';
+// import { SlashCommand } from 'ckeditor5-premium-features';
+
+import 'ckeditor5/ckeditor5.css';
+import 'ckeditor5-premium-features/ckeditor5-premium-features.css';
+
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <CKEditor
+      editor={ClassicEditor}
+      config={{
+        toolbar: {
+          items: [
+            'undo',
+            'redo',
+            '|',
+            'heading',
+            '|',
+            'bold',
+            'italic',
+            'underline',
+            '|',
+            'link',
+            'uploadImage',
+            'ckbox',
+            'insertTable',
+            'blockQuote',
+            'mediaEmbed',
+            '|',
+            'bulletedList',
+            'numberedList',
+            '|',
+            'outdent',
+            'indent',
+          ],
+        },
+        plugins: [
+          Autoformat,
+          BlockQuote,
+          Bold,
+          CloudServices,
+          Essentials,
+          Heading,
+          ckImage,
+          ImageCaption,
+          ImageResize,
+          ImageStyle,
+          ImageToolbar,
+          ImageUpload,
+          Base64UploadAdapter,
+          Indent,
+          IndentBlock,
+          Italic,
+          Link,
+          List,
+          MediaEmbed,
+          Mention,
+          Paragraph,
+          PasteFromOffice,
+          PictureEditing,
+          Table,
+          TableColumnResize,
+          TableToolbar,
+          TextTransformation,
+          Underline,
+          SimpleUploadAdapter,
+        ],
+        // mention: {
+        //   dropdownLimit:3,
+        //
+        //   // Mention configuration
+        // },
+        initialData: '<p>Hello from CKEditor 5 in React!</p>',
+        simpleUpload: {
+          // The URL that the images are uploaded to.
+          uploadUrl: 'http://example.com',
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+          // Enable the XMLHttpRequest.withCredentials property.
+          withCredentials: true,
+
+          // Headers sent along with the XMLHttpRequest to the upload server.
+          headers: {
+            'X-CSRF-TOKEN': 'CSRF-Token',
+            Authorization: 'Bearer <JSON Web Token>'
+          },
+
+          /* should return obj like { url } or {  "urls": {
+                   "default": "https://example.com/images/foo.jpg",
+                   "800": "https://example.com/images/foo-800.jpg",
+                   "1024": "https://example.com/images/foo-1024.jpg",
+                   "1920": "https://example.com/images/foo-1920.jpg"
+               }
+          */
+        },
+        heading: {
+          options: [
+            {
+              model: 'paragraph',
+              title: 'Paragraph',
+              class: 'ck-heading_paragraph',
+            },
+            {
+              model: 'heading1',
+              view: 'h1',
+              title: 'Heading 1',
+              class: 'ck-heading_heading1',
+            },
+            {
+              model: 'heading2',
+              view: 'h2',
+              title: 'Heading 2',
+              class: 'ck-heading_heading2',
+            },
+            {
+              model: 'heading3',
+              view: 'h3',
+              title: 'Heading 3',
+              class: 'ck-heading_heading3',
+            },
+            {
+              model: 'heading4',
+              view: 'h4',
+              title: 'Heading 4',
+              class: 'ck-heading_heading4',
+            },
+          ],
+        },
+        image: {
+          resizeOptions: [
+            {
+              name: 'resizeImage:original',
+              label: 'Default image width',
+              value: null,
+            },
+            {
+              name: 'resizeImage:50',
+              label: '50% page width',
+              value: '50',
+            },
+            {
+              name: 'resizeImage:75',
+              label: '75% page width',
+              value: '75',
+            },
+          ],
+          toolbar: [
+            'imageTextAlternative',
+            'toggleImageCaption',
+            '|',
+            'imageStyle:inline',
+            'imageStyle:wrapText',
+            'imageStyle:breakText',
+            '|',
+            'resizeImage',
+          ]
+        },
+
+      }}
+
+    />
   );
 }
